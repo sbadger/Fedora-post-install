16e23273f8676c2b14babe3603362c299dc4aa73
Shawn Badger <sbadger@users.noreply.github.com>
2019-04-04 07:12:24 -0700

    Update rpm-fusion.sh
    Changed it so the version number is automagically put in.

--- rpm-fusion.sh
+++ rpm-fusion.sh
@@ -1,6 +1,6 @@
 # install the rpm fusion packages
 
-dnf -y install --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-24.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-24.noarch.rpm
+dnf -y install --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 
 #install the other desktops for choice
