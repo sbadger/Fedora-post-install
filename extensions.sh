87b543aaf6b409682c8ffca1eea34e755c6b1086
Shawn Badger <sbadger@users.noreply.github.com>
2023-05-12 08:28:48 -0700

    Create extensions.sh

---
+++ extensions.sh
@@ -0,0 +1,4 @@
+# disable checking the version info
+gsettings set org.gnome.shell disable-extension-version-validation true
+# re-enable user extensions
+gsettings set org.gnome.shell disable-user-extensions false
